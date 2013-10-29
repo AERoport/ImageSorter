8db4beb8079e6d9b59541f64327038fafa19d1c5
Eduard <askadullin@yandex.ru>
2013-10-30 00:23:34 +0600

    13

--- ImageSorter/src/ru/yangantau/imagesorter/MainApp.java
+++ ImageSorter/src/ru/yangantau/imagesorter/MainApp.java
@@ -8,8 +8,6 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.Collection;
-
 import javax.swing.AbstractListModel;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
